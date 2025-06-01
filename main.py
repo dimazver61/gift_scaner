8ec5ffbd64072dc387255f770daf54c44b5fa706
NO NAME <dimazver161@gmail.com>
2025-06-01 04:09:14 +0300

    0.1

---
+++ main.py
@@ -0,0 +1,58 @@
+import argparse
+import time
+
+import coloring
+
+from src.tg_client import TgClient
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("phone", help="Phone")
+    args = parser.parse_args()
+
+    client = TgClient(args.phone)
+
+    gifts = client.get_available_gifts()
+    cur_gifts_id = [g.id for g in gifts if g.remaining_count != 0]
+
+    while 1:
+        try:
+            gifts = client.get_available_gifts()
+            new_gifts = [g for g in gifts if g.remaining_count != 0]
+            new_gifts_id = [g.id for g in new_gifts]
+
+            for ngi in new_gifts_id:
+                if ngi not in cur_gifts_id:
+
+                    print("новый гифт")
+                    cur_gifts_id.append(ngi)
+                    gift = [ng for ng in new_gifts if ng.id == ngi][0]
+
+                    text = f"New Gift | {gift.star_count} STARS"
+
+                    time.sleep(2)
+                    client.tg.send_message(907409516, text)
+
+                    time.sleep(2)
+                    client.tg.send_message(112298936, text)
+
+        except Exception as e:
+            print(coloring.red(f"[!] Error: {str(e)}"))
+        finally:
+            time.sleep(10)
+
+    # purchase_result = client.tg.call_method("starTransactionTypeGiftPurchase", {
+    #     "owner_id": 907409516,
+    #     # "gift": gift
+    # })
+    # purchase_result.wait()
+    #
+    # if purchase_result.error:
+    #     print(purchase_result.error_info)
+
+    # gifts = [Gift(**g) for g in m.update["gifts"]]
+    # select_gifts = [g for g in gifts if g.remaining_count != 0 and g.upgrade_star_count != 0]
+    #
+    # for g in select_gifts:
+    #     print(g.sticker.emoji, g.total_count, g.star_count, g.remaining_count)
---
+++ src/tg_client.py
@@ -0,0 +1,122 @@
+import random
+import threading
+from urllib.parse import urlparse, parse_qs
+
+from telegram.client import Telegram
+
+from src.types.gifts import Gift
+
+
+class TgClient:
+    def __init__(self, phone, api_id=27107768, api_hash="18b0bf93dbf4d97d72024ca8ff40f66a"):
+        self.tg = Telegram(
+            api_id=api_id,
+            api_hash=api_hash,
+            phone=phone,  # you can pass 'bot_token' instead
+            database_encryption_key='',
+            files_directory='_temp/',
+            library_path="src/tdlib/tdjson.dll",
+            login=True
+        )
+        self.tg.get_chats(limit=500).wait()
+
+        r = self.tg.get_me()
+        r.wait()
+        self.is_premium = r.update["is_premium"]
+        self.slots_id = None
+
+        if self.is_premium:
+            self.slots_id = self.get_boost_slots_id()
+
+    def get_boost_slots_id(self):
+        r = self.tg.call_method("getAvailableChatBoostSlots")
+        r.wait()
+        slots = r.update["slots"]
+        return [s["slot_id"] for s in slots]
+
+    def boost_chat(self, chat_id):
+        # search_result = self.tg.call_method("searchPublicChat", params={"username": username})
+        # search_result.wait()
+        # chat_id = search_result.update['id']
+
+        slot_ids = [random.choice(self.slots_id)]
+        r = self.tg.call_method("boostChat", {
+            "chat_id": chat_id,
+            "slot_ids": slot_ids
+        })
+        r.wait()
+        if r.error:
+            raise Exception(r.error_info)
+
+    def join_channel(self, username):
+        search_result = self.tg.call_method("searchPublicChat", params={"username": username})
+        search_result.wait()
+        if search_result.error:
+            raise Exception(search_result.error_info)
+
+        chat_id = search_result.update["id"]
+        r = self.tg.call_method("joinChat", {"chat_id": chat_id})
+        r.wait()
+        if r.error:
+            raise Exception(r.error_info)
+        return chat_id
+
+    def get_web_app_init_data(self):
+        search_result = self.tg.call_method("searchPublicChat", params={"username": "mrkt"})
+        search_result.wait()
+
+        r = self.tg.call_method('openWebApp', params={
+            "chat_id": 8156315866,
+            "bot_user_id": 8156315866,
+            "url": "https://t.me/mrkt/app",
+            "message_thread_id": 0,
+            "reply_to": None,
+            "parameters": {
+                "@type": "webAppOpenParameters",
+                "theme": None,
+                "application_name": "app",
+                "mode": None
+            }
+        })
+        r.wait()
+        url = r.update['url']
+        parsed_url = urlparse(url)
+        query_params = parse_qs(parsed_url.fragment)
+        return query_params["tgWebAppData"][0]
+
+    def get_available_gifts(self) -> list[Gift]:
+        m = self.tg.call_method("getAvailableGifts")
+        m.wait()
+
+        return [Gift(**g) for g in m.update["gifts"]]
+
+    def send_message(self, chat_id, text):
+        send_message_result = self.tg.send_message(
+            chat_id=chat_id,
+            text=text,
+        )
+        send_message_result.wait()
+
+        if send_message_result.error:
+            print(f"Failed to send the message: {send_message_result.error_info}")
+
+        message_has_been_sent = threading.Event()
+
+        def update_message_send_succeeded_handler(update):
+            print(f"Received updateMessageSendSucceeded: {update}")
+            # When we sent the message, it got a temporary id. The server assigns permanent id to the message
+            # when it receives it, and tdlib sends the updateMessageSendSucceeded event with the new id.
+            #
+            # Check that this event is for the message we sent.
+            if update["old_message_id"] == send_message_result.update["id"]:
+                new_message_id = update["message"]["id"]
+                print(f"Message has been sent. New message id: {new_message_id}")
+                message_has_been_sent.set()
+
+            # When the event is received, the handler is called.
+
+        self.tg.add_update_handler("updateMessageSendSucceeded", update_message_send_succeeded_handler)
+
+        # Wait for the message to be sent
+        message_has_been_sent.wait(timeout=60)
+        print("Message has been sent.")
